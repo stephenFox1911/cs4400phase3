4c4cfeba2350638cc5a91c384800dd2ffbaf0fd9
jwjibilian <jwjibilian@gmail.com>
2015-04-19 15:44:55 -0400

    Copy Field In DamageLostBook

--- Views/src/TaskViews/DamagedLostBook.java
+++ Views/src/TaskViews/DamagedLostBook.java
@@ -27,6 +27,7 @@ public class DamagedLostBook extends JPanel {
     private JTextField txtLastuser;
     private JTextField txtChargeammount;
     private String penaltyUsername;
+    private JTextField txtCopynumber;
     /**
      * Create the panel.
      */
@@ -34,9 +35,9 @@ public class DamagedLostBook extends JPanel {
         containedIn = in;
         GridBagLayout gridBagLayout = new GridBagLayout();
         gridBagLayout.columnWidths = new int[]{5, 0, 0, 129, 31, 0, 0};
-        gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-        gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
         setLayout(gridBagLayout);
         
         JLabel lblLostdamagedBook = new JLabel("Lost/Damaged Book");
@@ -65,12 +66,30 @@ public class DamagedLostBook extends JPanel {
         add(textISBN, gbc_textISBN);
         textISBN.setColumns(10);
         
+        JLabel lblCopyNumber = new JLabel("Copy Number:");
+        GridBagConstraints gbc_lblCopyNumber = new GridBagConstraints();
+        gbc_lblCopyNumber.anchor = GridBagConstraints.EAST;
+        gbc_lblCopyNumber.insets = new Insets(0, 0, 5, 5);
+        gbc_lblCopyNumber.gridx = 1;
+        gbc_lblCopyNumber.gridy = 3;
+        add(lblCopyNumber, gbc_lblCopyNumber);
+        
+        txtCopynumber = new JTextField();
+        GridBagConstraints gbc_txtCopynumber = new GridBagConstraints();
+        gbc_txtCopynumber.gridwidth = 2;
+        gbc_txtCopynumber.insets = new Insets(0, 0, 5, 5);
+        gbc_txtCopynumber.fill = GridBagConstraints.HORIZONTAL;
+        gbc_txtCopynumber.gridx = 2;
+        gbc_txtCopynumber.gridy = 3;
+        add(txtCopynumber, gbc_txtCopynumber);
+        txtCopynumber.setColumns(10);
+        
         JLabel lblCurrentTime = new JLabel("Current Time:");
         GridBagConstraints gbc_lblCurrentTime = new GridBagConstraints();
         gbc_lblCurrentTime.anchor = GridBagConstraints.EAST;
         gbc_lblCurrentTime.insets = new Insets(0, 0, 5, 5);
         gbc_lblCurrentTime.gridx = 1;
-        gbc_lblCurrentTime.gridy = 3;
+        gbc_lblCurrentTime.gridy = 4;
         add(lblCurrentTime, gbc_lblCurrentTime);
         
         txtCurrenttime = new JTextField();
@@ -80,7 +99,7 @@ public class DamagedLostBook extends JPanel {
         gbc_txtCurrenttime.insets = new Insets(0, 0, 5, 5);
         gbc_txtCurrenttime.fill = GridBagConstraints.HORIZONTAL;
         gbc_txtCurrenttime.gridx = 2;
-        gbc_txtCurrenttime.gridy = 3;
+        gbc_txtCurrenttime.gridy = 4;
         add(txtCurrenttime, gbc_txtCurrenttime);
         txtCurrenttime.setColumns(10);
         
@@ -89,7 +108,7 @@ public class DamagedLostBook extends JPanel {
         gbc_btnLookForLastUser.gridwidth = 4;
         gbc_btnLookForLastUser.insets = new Insets(0, 0, 5, 5);
         gbc_btnLookForLastUser.gridx = 1;
-        gbc_btnLookForLastUser.gridy = 4;
+        gbc_btnLookForLastUser.gridy = 5;
         add(btnLookForLastUser, gbc_btnLookForLastUser);
         
         JLabel lblLastUser = new JLabel("Last User:");
@@ -97,7 +116,7 @@ public class DamagedLostBook extends JPanel {
         gbc_lblLastUser.anchor = GridBagConstraints.EAST;
         gbc_lblLastUser.insets = new Insets(0, 0, 5, 5);
         gbc_lblLastUser.gridx = 1;
-        gbc_lblLastUser.gridy = 5;
+        gbc_lblLastUser.gridy = 6;
         add(lblLastUser, gbc_lblLastUser);
         
         txtLastuser = new JTextField();
@@ -107,7 +126,7 @@ public class DamagedLostBook extends JPanel {
         gbc_txtLastuser.insets = new Insets(0, 0, 5, 5);
         gbc_txtLastuser.fill = GridBagConstraints.HORIZONTAL;
         gbc_txtLastuser.gridx = 2;
-        gbc_txtLastuser.gridy = 5;
+        gbc_txtLastuser.gridy = 6;
         add(txtLastuser, gbc_txtLastuser);
         txtLastuser.setColumns(10);
         
@@ -116,7 +135,7 @@ public class DamagedLostBook extends JPanel {
         gbc_lblAmmountToCharge.anchor = GridBagConstraints.EAST;
         gbc_lblAmmountToCharge.insets = new Insets(0, 0, 5, 5);
         gbc_lblAmmountToCharge.gridx = 1;
-        gbc_lblAmmountToCharge.gridy = 6;
+        gbc_lblAmmountToCharge.gridy = 7;
         add(lblAmmountToCharge, gbc_lblAmmountToCharge);
         
         txtChargeammount = new JTextField();
@@ -125,7 +144,7 @@ public class DamagedLostBook extends JPanel {
         gbc_txtChargeammount.insets = new Insets(0, 0, 5, 5);
         gbc_txtChargeammount.fill = GridBagConstraints.HORIZONTAL;
         gbc_txtChargeammount.gridx = 2;
-        gbc_txtChargeammount.gridy = 6;
+        gbc_txtChargeammount.gridy = 7;
         add(txtChargeammount, gbc_txtChargeammount);
         txtChargeammount.setColumns(10);
         
@@ -134,14 +153,14 @@ public class DamagedLostBook extends JPanel {
         gbc_btnSubmit.anchor = GridBagConstraints.EAST;
         gbc_btnSubmit.insets = new Insets(0, 0, 0, 5);
         gbc_btnSubmit.gridx = 3;
-        gbc_btnSubmit.gridy = 7;
+        gbc_btnSubmit.gridy = 8;
         add(btnSubmit, gbc_btnSubmit);
         
         JButton btnBack = new JButton("Back");
         GridBagConstraints gbc_btnBack = new GridBagConstraints();
         gbc_btnBack.insets = new Insets(0, 0, 0, 5);
         gbc_btnBack.gridx = 4;
-        gbc_btnBack.gridy = 7;
+        gbc_btnBack.gridy = 8;
         add(btnBack, gbc_btnBack);
         
         btnSubmit.addActionListener(new ActionListener() {
